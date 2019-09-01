2eca35e394222020678041c6d45708b5d0f61145
wfqtyhj@gmail.com <wfqtyhj@gmail.com>
2019-09-01 17:12:14 +0800

    新增用户

--- spk-data/src/main/java/cn/spk/data/controller/UserController.java
+++ spk-data/src/main/java/cn/spk/data/controller/UserController.java
@@ -1,8 +1,10 @@
 package cn.spk.data.controller;
 
+import cn.spk.data.conf.ClientUserConfigProperties;
 import cn.spk.data.entity.FrameUser;
 import cn.spk.data.util.Util;
 import com.alibaba.fastjson.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,11 +18,15 @@ public class UserController {
     @Resource
     private RestTemplate restTemplate;
 
+    @Autowired
+    private ClientUserConfigProperties clientUserConfigProperties;
+
     @GetMapping("/getUsers")
     public List<FrameUser> getUsers() {
-        String url = "http://localhost:8764/spkuser/frameUserController/getUsers";
+        String url = clientUserConfigProperties.getUrl();
         String body = Util.httpExchangeGet(url, restTemplate);
         return JSON.parseArray(body, FrameUser.class);
     }
+
 
 }
