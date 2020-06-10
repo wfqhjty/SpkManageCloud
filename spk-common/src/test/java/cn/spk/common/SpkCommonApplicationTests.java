b040f28121be8a986be7f871cf9af7c8bbbfa676
wangfq <wfqtyhj@gmail.com>
2020-06-10 22:15:55 +0800

    init

---
+++ spk-common/src/test/java/cn/spk/common/SpkCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.spk.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpkCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
